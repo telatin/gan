0de301ec77aea25d784d6fcb16ff3566cb201441
Andrea Telatin <andrea@telatin.com>
2020-09-30 21:05:28 +0100

    v. 0.1

---
+++ scripts/ganlib.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+version = "0.1.0"
+columns = ["Language", "Gender", "Part", "Word", "Definition", "Explanation"]
+
+validate = {
+    "Language": ["L.", "Gr.", "N.L.", "M.E.", "M.L."],
+    "Gender": ["masc.", "fem.", "neut.", "masc./fem."],
+    "Part": ["n.", "adj.", "adv.", "gen.", "nom."]   #?
+}
+
+# abl.
+# ablative
+# adj.
+# adjective
+# adv.
+# adverb
+# Chem.
+# Chemical term
+# comp.
+# comparative
+# dim.
+# diminutive
+# fem.
+# feminine gender
+# gen.
+# genitive case
+# Gr.
+# latinized Greek (the original Greek spelling is not given and the word is transliterated into the Latin alphabet)
+# L.
+# Latin (indicates that the word is classic Latin and found in an unabridged Latin dictionary)
+# masc.
+# masculine gender
+# M.E.
+# Middle English
+# M.L.
+# Medieval (sometimes pharmaceutical) Latin
+# n.
+# noun
+# neut.
+# neuter gender
+# N.Gr.
+# Neo-Greek (modern Greek)
+# N.L.
+# Neo-Latin (a word treated and used as a Latin word)
+# nom.
+# nominative case
+# num.
+# numerus
+# part. adj.
+# participle adjective
+# part.
+# participle
+# pass.
+# passive
+# perf.
+# perfect
+# pl.
+# plural
+# pref.
+# prefix
+# prep.
+# preposition
+# pres. part.
+# present participle
+# pron.
+# pronoun
+# sing.
+# singular
+# suff.
+# suffix
+# sup.
+# superlative
+# sync.
+# syncope
+# v.
+# verb
---
+++ scripts/generateGenus_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import itertools
+
+# each term is a list of lists, as some terms can be declined in more 
+# than one way but still being the same (e.g ornis/ornithos)
+pref  = [ ['pullus'], ['ornis','ornithos'], ['avis'], ['kottos'], ['gallus', 'gallu'], ['alektryon'], ['pteron'], ['gallina']]
+mid   = [ ['stercus'],['excrementum'],['kopros'], ['faex','faecis'], ['merda'],['kakke'],['enteron'],['intestinum'],[''] ]
+terms = [ ['cola'], ['microbium'],['monas'],['spira'],['bios'],['bacterium'],['plasma'],['soma'],[''] ]
+
+
+# the 'iterables' list contains the parts of the word to be generated in the order they should appear
+iterables = [ pref, mid, terms ]
+
+counter = 0
+
+# itertools.product generates all the combinations of the lists in "iterables"
+for t in itertools.product(*iterables):
+    spellingVariants = []
+    for subList in t:
+    	spellingVariants.append(subList)
+    counter += 1
+    print(counter, end="\t")
+    for j in itertools.product(*spellingVariants):
+        print( ''.join(j), end=", ")
+
+    print("")
+       
